27ecdd99260181907abd3b85fd123080910fc9ed
Влад Рогов <alexfresh700@gmail.com>
2022-10-05 17:08:48 +0300

    привет + имя

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя) ");
+string user_name = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(user_name);
